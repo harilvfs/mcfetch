01003dae9353e5abc77c625c5bfd12c280a27944
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-09-15 20:25:13 +0300

    feat(main): Add color support

--- src/main.rs
+++ src/main.rs
@@ -24,13 +24,33 @@ impl SystemInfo {
         }
     }
 
-    fn display(&self) -> String {
+    fn display(&self, color: &str, bold: bool) -> String {
+        let reset = "\x1b[0m";
+        let color_escape = match color {
+            "black" => "\x1b[30m",
+            "red" => "\x1b[31m",
+            "green" => "\x1b[32m",
+            "yellow" => "\x1b[33m",
+            "blue" => "\x1b[34m",
+            "magenta" => "\x1b[35m",
+            "cyan" => "\x1b[36m",
+            "light_black" => "\x1b[90m",
+            "light_red" => "\x1b[91m",
+            "light_green" => "\x1b[92m",
+            "light_yellow" => "\x1b[93m",
+            "light_blue" => "\x1b[94m",
+            "light_magenta" => "\x1b[95m",
+            "light_cyan" => "\x1b[96m",
+            _ => "\x1b[32m",
+        };
+
+        let bold_escape = match bold {
+            true => "\x1b[1m",
+            false => "",
+        };
+
         format!(
-            "
-        {}{}{}
-        System: {}
-        Kernel version: {}
-        ",
+            "{bold_escape}{color_escape}{}{reset}{}{bold_escape}{color_escape}{}\nSystem: {reset}{}{bold_escape}{color_escape}\nKernel version: {reset}{}",
             self.username, "@", self.hostname, self.os, self.kernel_version
         )
     }
@@ -38,5 +58,5 @@ impl SystemInfo {
 
 fn main() {
     let system_info = SystemInfo::new();
-    println!("{}", system_info.display());
+    println!("{}", system_info.display("green", true));
 }
