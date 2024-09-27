7e81ae7ddc433acb37d05a18f0a438e85585f224
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-09-27 21:42:39 +0300

    feat(pkg_counter): Add new package listers

--- src/pkg_counter.rs
+++ src/pkg_counter.rs
@@ -1,7 +1,11 @@
+use std::collections::HashMap;
+use std::iter::FromIterator;
 use std::process::Command;
 
 fn is_installed(command: &str) -> bool {
-    let pkg = Command::new(command).output();
+    let (program, args) = command.split_once(char::is_whitespace).unwrap();
+    let args: Vec<&str> = args.split(char::is_whitespace).collect();
+    let pkg = Command::new(program).args(&args).output();
 
     match pkg {
         Ok(_) => true,
@@ -16,11 +20,17 @@ pub struct PackageManager {
 
 impl PackageManager {
     pub fn get_pkg_manager() -> String {
-        let pkg_managers = vec!["dnf", "pacman", "apt"];
+        let pkg_managers: HashMap<&str, &str> = HashMap::from_iter([
+            ("pacman", "pacman -V"),
+            ("apk", "apk -V"),
+            ("xbps", "xbps-query -V"),
+            ("dnf", "dnf --veresion"),
+            ("apt", "apt -v"),
+        ]);
 
-        for m in pkg_managers {
-            if is_installed(m) {
-                return m.to_string();
+        for (name, try_command) in &pkg_managers {
+            if is_installed(try_command) {
+                return name.to_string();
             }
         }
 
@@ -29,6 +39,39 @@ impl PackageManager {
 
     pub fn get_count() -> u32 {
         match PackageManager::get_pkg_manager().as_str() {
+            "xbps" => {
+                let output_raw = Command::new("xbps-query")
+                    .arg("-l")
+                    .output()
+                    .unwrap()
+                    .stdout;
+                let output = String::from_utf8(output_raw)
+                    .expect("get_count: Failed to convert stdout to string");
+                let mut count = 0;
+
+                for line in output.lines() {
+                    if line.starts_with("ii") {
+                        count += 1;
+                    }
+                }
+                count
+            }
+            "apk" => {
+                let output_raw = Command::new("apk")
+                    .arg("list")
+                    .arg("-I")
+                    .output()
+                    .unwrap()
+                    .stdout;
+                let output = String::from_utf8(output_raw)
+                    .expect("get_count: Failed to convert stdout to string");
+                let mut count = 0;
+
+                for _ in output.lines() {
+                    count += 1;
+                }
+                count
+            }
             "dnf" => {
                 let output_raw = Command::new("dnf")
                     .arg("repoquery")
@@ -47,13 +90,8 @@ impl PackageManager {
                 }
                 count
             }
-            "apt" => {
-                let output_raw = Command::new("apt")
-                    .arg("list")
-                    .arg("--installed")
-                    .output()
-                    .unwrap()
-                    .stdout;
+            "pacman" => {
+                let output_raw = Command::new("pacman").arg("-Qq").output().unwrap().stdout;
                 let output = String::from_utf8(output_raw)
                     .expect("get_count: Failed to convert stdout to string");
                 let mut count = 0;
@@ -63,8 +101,13 @@ impl PackageManager {
                 }
                 count
             }
-            "pacman" => {
-                let output_raw = Command::new("pacman").arg("-Q").output().unwrap().stdout;
+            "apt" => {
+                let output_raw = Command::new("apt")
+                    .arg("list")
+                    .arg("--installed")
+                    .output()
+                    .unwrap()
+                    .stdout;
                 let output = String::from_utf8(output_raw)
                     .expect("get_count: Failed to convert stdout to string");
                 let mut count = 0;
