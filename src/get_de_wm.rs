79e4504d2f7cabb62d444687260e3ae558c246c5
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-09-29 01:11:04 +0300

    refactor(get_de_wm): Replace bash scripting

--- src/get_de_wm.rs
+++ src/get_de_wm.rs
@@ -1,4 +1,5 @@
 use crate::essentials::process_grep;
+use regex::Regex;
 use std::env;
 use std::process::Command;
 
@@ -44,9 +45,37 @@ pub fn main() -> String {
 
         "Unknown".to_string()
     } else if env::var("DISPLAY").is_ok() {
-        let wm_output = String::from_utf8(Command::new("bash").arg("-c").arg(r#"id=$(xprop -root -notype _NET_SUPPORTING_WM_CHECK) && id=${id##* } && wm=$(xprop -id "$id" -notype -len 100 -f _NET_WM_NAME 8t) && wm=${wm/*WM_NAME = } && wm=${wm/\"} && wm=${wm/\"*} && printf $wm"#).output().unwrap().stdout)
-            .expect("get_de_wm: Failed to convert stdout to string");
-        wm_output
+        let wm_id_re = Regex::new(r"0x[0-9a-fA-F]+").unwrap();
+        let wm_id_command = String::from_utf8(
+            Command::new("xprop")
+                .args(["-root", "-notype", "_NET_SUPPORTING_WM_CHECK"])
+                .output()
+                .unwrap()
+                .stdout,
+        )
+        .expect("wm_id_command: Cannot convert bytes to UTF8");
+        let wm_id = wm_id_re.captures(wm_id_command.as_str()).unwrap();
+
+        let wm_name_re = Regex::new(r#"_NET_WM_NAME\s*=\s*"([^"]+)""#).unwrap();
+        let wm_name_command = String::from_utf8(
+            Command::new("xprop")
+                .args([
+                    "-id",
+                    &wm_id[0],
+                    "-notype",
+                    "-len",
+                    "100",
+                    "-f",
+                    "_NET_WM_NAME",
+                    "8t",
+                ])
+                .output()
+                .unwrap()
+                .stdout,
+        )
+        .expect("wm_name_command: Cannot convert bytes to UTF8");
+        let wm_name = wm_name_re.captures(wm_name_command.as_str()).unwrap();
+        wm_name[1].to_string()
     } else {
         "Unknown".to_string()
     }
